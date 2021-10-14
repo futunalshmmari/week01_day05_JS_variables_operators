b346ee7f06d24bc78de7fde22803d3c723f67d42
Futon Alshmmari <futon1921@gmail.com>
2021-10-14 19:16:15 +0300

    Three assignments on file

---
+++ page2.js
@@ -0,0 +1,16 @@
+//The Fortune Teller
+console .log("....The Fortune Teller....")
+let num1 = 0;
+let partnername ="g";
+let geolocation = prompt("Where do you live?")
+console .log("children.."+"  "+num1+"  "+ " partner's name..  "+partnername+"   " +"live.."+geolocation );
+console .log("..The Age Calculator..")
+//The Age Calculator
+let h1=prompt("the year")
+let h2=prompt("Year of Birth")
+console.log("Age",h2-h1);
+//The Geometrizer
+console .log("..The Geometrizer..")
+let h3=prompt("Enter the radius")
+console.log("circumference=",2*3.14159*h3);
+console.log("Area=",3141517*h3*h3);
