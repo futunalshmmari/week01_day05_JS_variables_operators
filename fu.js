36686d1b7d5d7e250d5fe31d41ba36cb7ce81312
Futon Alshmmari <futon1921@gmail.com>
2021-10-18 23:16:54 +0300

    Bonus functions

---
+++ fu.js
@@ -0,0 +1,31 @@
+console .log("....The Fortune Teller....")
+//let num1 = 0;
+//let partnername ="g";
+function f1(num1,partnername,geolocation){
+//let geolocation = prompt("Where do you live?")
+console .log("children.."+"  "+num1+"  "+ " partner's name..  "+partnername+"   " +"live.."+geolocation );}
+console .log("..The Age Calculator..")
+//The Age Calculator
+//let h1=prompt("the year")
+//let h2=prompt("Year of Birth")
+function f2(h1,h2){
+console.log("Age",h2-h1);}
+//The Geometrizer
+console .log("..The Geometrizer..")
+//let h3=prompt("Enter the radius")
+function f3(h3){
+console.log("circumference=",2*3.14159*h3);
+console.log("Area=",3141517*h3*h3);}
+////////////function
+let num1 = 0;
+let partnername ="g";
+let geolocation = prompt("Where do you live?")
+f1(num1,partnername,geolocation);
+let h1=prompt("the year")
+let h2=prompt("Year of Birth")
+f2(h1,h2);
+let h3=prompt("Enter the radius")
+f3(h3);
+
+
+
